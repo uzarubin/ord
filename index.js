6ce540092c876c3a2c5e126b59de2f5b685457c0
Ustin Zarubin <ustin@shimmur.com>
2018-05-13 22:04:18 -0400

    commit hook working?

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { AppRegistry } from 'react-native';
-import App from './src/containers';
+import { AppRegistry } from 'react-native'
+import App from './src/containers'
 
-AppRegistry.registerComponent('ord', () => App);
+AppRegistry.registerComponent('ord', () => App)
